4b2fcc2112c589f4d6bcc9872ad760901fbb1a6c
Adam Lopez <adamf04@gmail.com>
2025-03-27 14:35:22 -0400

    Commenting out tours and adding the products component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
 import Services from "./Components/Services";
-import Tours from "./Components/Tours";
+// import Tours from "./Components/Tours";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Hero />
       <About />
       <Services />
-      <Tours />
+      {/* <Tours /> */}
+      <Products />
       <Footer />
     </>
   );
